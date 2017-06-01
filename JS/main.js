9c942c8bfa66636af8f98150afa034e50bb0356b
Michael Ashton <mykl.ashton@gmail.com>
2017-06-01 10:14:29 -0400

    Upload week one project in totem

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+
+function main() {
+  $('.items').hide();
+  $('.item7').fadeIn(1000);
+  $('.item6').fadeIn(2000);
+  $('.item5').fadeIn(3000);
+  $('.item4').fadeIn(4000);
+  $('.item3').fadeIn(4500);
+  $('.item2').fadeIn(5000);
+  $('.item1').fadeIn(5300);
+}
+
+
+
+$(document).ready(main);
